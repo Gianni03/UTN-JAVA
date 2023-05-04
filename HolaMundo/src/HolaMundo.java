3ff7b16f3801fee0be68aae43631b64d0e3ac3de
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2023-05-04 19:48:06 -0300

    primer hola mundo en java, mas variables primitivas

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @author gianni
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        
+        //variable numerica
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        
+        //vaariable caracteres
+        String miString = "Aprendiendo JAVA";
+        System.out.println(miString);
+        miString = "bueno, esto es JAVA";
+        System.out.println(miString);
+        
+    }
+}
